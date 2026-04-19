1ebbff7665502f6e7f9c4c7aaade902f05e28f07
GADGETNiK <playgadgetnik@inbox.ru>
2026-04-20 00:41:15 +0300

    Switch MI to IN vendor OS1.0.8.0

--- Android.mk
+++ Android.mk
@@ -6,14 +6,14 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),fire)
 
-$(call add-radio-file-sha1-checked,radio/gz.img,682719512596824b18be262871a848ab54ba7235)
-$(call add-radio-file-sha1-checked,radio/lk.img,0438b3e957e40d07a89db323f7729c6e48f8c7ec)
-$(call add-radio-file-sha1-checked,radio/logo.img,a8875732142905aad844cc0e902aeb1fcf70cb05)
-$(call add-radio-file-sha1-checked,radio/md1img.img,1983af5e75db0fb8f03e2c461e9aed56d52f0718)
-$(call add-radio-file-sha1-checked,radio/preloader_raw.img,1c100c9b967943932310f491c41d8e3adb40f293)
-$(call add-radio-file-sha1-checked,radio/scp.img,c2fdd9d6b468f604dd913fd2485340213c42da6e)
-$(call add-radio-file-sha1-checked,radio/spmfw.img,cfac9cad775e1b67ee608c298c7d34b4e82d4854)
-$(call add-radio-file-sha1-checked,radio/sspm.img,71de9fc283fea407ff7fa07b32becec463650f28)
-$(call add-radio-file-sha1-checked,radio/tee.img,3a65a7bee9d59b4110259381dc09ba37b0a51111)
+$(call add-radio-file-sha1-checked,radio/gz.img,8ed7b01425866c9d4d70a6169f4a2f0bae5de40e)
+$(call add-radio-file-sha1-checked,radio/lk.img,c47093db999672c2acb40363baf3cad30c963c9d)
+$(call add-radio-file-sha1-checked,radio/logo.img,b36bf935524973842258ae528b60e0e675336097)
+$(call add-radio-file-sha1-checked,radio/md1img.img,c2870baba934cd778936b56148033c57a3e6d037)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,7271703c3a689c2639219289dbc52427ae11d4ec)
+$(call add-radio-file-sha1-checked,radio/scp.img,ad2c6ae6598defcf5dede7a514cca9d44d965e9c)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,f3a7e1656335313bb796656985856f754fb43b4c)
+$(call add-radio-file-sha1-checked,radio/sspm.img,997a97d1d84b7394c5f746bbfd8dd2863e81f574)
+$(call add-radio-file-sha1-checked,radio/tee.img,772f2c9cea88a2db696217727ec5ce487fef342b)
 
 endif
